bd3e762f8bf44cb7ea490558b2a7a863c105690f
Charlon Palacay <charlon@gmail.com>
2017-03-22 09:07:26 -0700

    Use a slide down css transition for show and hide.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 	$("#menu_toggle").click(function(){
-		$("#menu_container").toggleClass("visible");
+		$("#menu_container").toggleClass("slide-down");
 	});
 
     $("#search_toggle").click(function(){
-		$("#search_container").toggleClass("visible");
+		$("#search_container").toggleClass("slide-down");
 	});
 
 });
